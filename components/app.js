e3a8203b38071f4dc32f3bd7c0ea2de206ac8cce
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-01-19 22:27:12 +0100

    Add styles & fix minor mistakes in scripts

--- components/app.js
+++ components/app.js
@@ -4,37 +4,30 @@ var contacts = [
     firstName: 'Jan',
     lastName: 'Nowak',
     email: 'jan.nowak@example.com',
+    src: 'https://www.shareicon.net/data/128x128/2016/09/01/822711_user_512x512.png'
   },
   {
     id: 2,
-    firstName: 'Adam',
-    lastName: 'Kowalski',
-    email: 'adam.kowalski@example.com',
+    firstName: 'Ida',
+    lastName: 'Kowalska',
+    email: 'ida.kowalska@example.com',
+    src: 'https://www.shareicon.net/data/128x128/2016/09/01/822761_user_512x512.png'
   },
   {
     id: 3,
-    firstName: 'Zbigniew',
-    lastName: 'Koziol',
-    email: 'zbigniew.koziol@example.com',
+    firstName: 'Anna',
+    lastName: 'Zalewska',
+    email: 'anna.zalewska@example.com',
+    src: 'https://www.shareicon.net/data/128x128/2016/09/01/822727_user_512x512.png'
   }
 ];
 
 var contactForm = {
   firstName: '',
   lastName: '',
-  email: ''
+  email: '',
+  src: ''
 };
-
-var App = React.createClass({
-  render: function() {
-    return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
-      )
-    );
-  }
-});
 
 var App = React.createClass({
   render: function() {
--- components/contact.js
+++ components/contact.js
@@ -8,10 +8,10 @@ var Contact = React.createClass({
       React.createElement('div', {className: 'contactItem'},
         React.createElement('img', {
           className: 'contactImage',
-          src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
+          src: this.props.item.src
         }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, this.props.item.lastName),
         React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
           this.props.item.email
         )
